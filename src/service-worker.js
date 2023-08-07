cdb04189becb673f4dabd597347e41d34a0c5c35
goto sho <gotuu91.afl@gmail.com>
2023-08-07 15:31:03 +0900

    Fix TypeError on the top page #107

--- src/service-worker.js
+++ src/service-worker.js
@@ -34,7 +34,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
 	// ignore POST requests etc
 	if (event.request.method !== 'GET') return
-	if (event.request.url.indexOf('http') !== 0) return
+	if (!event.request.url.startsWith('http')) return
 
 	async function respond() {
 		const url = new URL(event.request.url)
