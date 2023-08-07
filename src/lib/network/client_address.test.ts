d4848b01aacad312b186f5db7b7339c5c7b3caba
sinYa Iwasaki <iwasaki@sinproject.net>
2023-08-07 13:52:41 +0900

    Setup test coverage for sonarcloud #202

--- src/lib/network/client_address.test.ts
+++ src/lib/network/client_address.test.ts
@@ -1,34 +1,38 @@
-// import { test, expect } from 'vitest'
-// import { ClientAddress } from './client_address'
-// import dotenv from 'dotenv'
+import { test, expect } from 'vitest'
+import { ClientAddress } from './client_address'
+import dotenv from 'dotenv'
 
-// dotenv.config()
+dotenv.config()
 
-// test('ClientAddress should return x-forwarded-for header if it exists', () => {
-// 	const mock_request = {
-// 		headers: {
-// 			get: (name: string) => {
-// 				if (name === 'x-forwarded-for') return '123.456.789.101'
-// 				return null
-// 			},
-// 		},
-// 	} as unknown as Request
+test('ClientAddress should return x-forwarded-for header if it exists', () => {
+	if (process.env.CI) return
 
-// 	const client_address = new ClientAddress(mock_request, () => process.env.TEST_IP_ADDRESS ?? '')
-// 	const result = client_address.value
+	const mock_request = {
+		headers: {
+			get: (name: string) => {
+				if (name === 'x-forwarded-for') return '123.456.789.101'
+				return null
+			},
+		},
+	} as unknown as Request
 
-// 	expect(result).toEqual('123.456.789.101')
-// })
+	const client_address = new ClientAddress(mock_request, () => process.env.TEST_IP_ADDRESS ?? '')
+	const result = client_address.value
 
-// test('ClientAddress should call _get_client_address function if x-forwarded-for header does not exist', () => {
-// 	const mock_request = {
-// 		headers: {
-// 			get: () => null,
-// 		},
-// 	} as unknown as Request
+	expect(result).toEqual('123.456.789.101')
+})
 
-// 	const client_address = new ClientAddress(mock_request, () => process.env.TEST_IP_ADDRESS ?? '')
-// 	const result = client_address.value
+test('ClientAddress should call _get_client_address function if x-forwarded-for header does not exist', () => {
+	if (process.env.CI) return
 
-// 	expect(result).toEqual(process.env.TEST_IP_ADDRESS)
-// })
+	const mock_request = {
+		headers: {
+			get: () => null,
+		},
+	} as unknown as Request
+
+	const client_address = new ClientAddress(mock_request, () => process.env.TEST_IP_ADDRESS ?? '')
+	const result = client_address.value
+
+	expect(result).toEqual(process.env.TEST_IP_ADDRESS)
+})
