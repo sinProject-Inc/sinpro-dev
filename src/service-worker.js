844e742d0f684a7ba6521df50510bce68d319dc1
sinYa Iwasaki <iwasaki@sinproject.net>
2023-08-08 16:50:00 +0900

    PR review by ChatGPT #212

--- src/service-worker.js
+++ src/service-worker.js
@@ -35,7 +35,7 @@ self.addEventListener('fetch', (event) => {
 	// ignore POST requests etc
 	if (event.request.method !== 'GET') return
 	if (event.request.url.indexOf('http') !== 0) return
-	// if (!event.request.url.startsWith('http')) return
+	// // if (!event.request.url.startsWith('http')) return
 
 	async function respond() {
 		const url = new URL(event.request.url)
