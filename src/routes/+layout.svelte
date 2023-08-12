74d26bf8b72728a7adc79c8829e329c05d783629
sinYa Iwasaki <iwasaki@sinproject.net>
2023-08-12 15:34:58 +0900

    Implement AdSense #135

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -124,6 +124,14 @@
 	})
 </script>
 
+<svelte:head>
+	<script
+		async
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4064604490139558"
+		crossorigin="anonymous"
+	></script>
+</svelte:head>
+
 <Audio />
 
 <div class={$theme ? 'visible' : 'invisible'}>
