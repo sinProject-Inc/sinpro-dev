6b50482294b2d9961fdcebd0c1615f2a6e1e1f78
sinYa Iwasaki <iwasaki@sinproject.net>
2023-08-25 10:31:20 +0900

    Refactor current_page_store.test.ts  #237

--- src/lib/docs/current_page_store.test.ts
+++ src/lib/docs/current_page_store.test.ts
@@ -1,17 +1,21 @@
-import { get } from 'svelte/store'
+import { get, type Writable } from 'svelte/store'
 import { expect, test } from 'vitest'
 import { current_page_category, current_page_title } from './current_page_store'
 
-test('current_page_title', () => {
-	expect(get(current_page_title)).toBe('')
+function test_store<T>(store: Writable<T>, initial_value: T, test_value: T): void {
+	expect(get(store)).toBe(initial_value)
 
-	current_page_title.set('Home')
-	expect(get(current_page_title)).toBe('Home')
-})
+	store.set(test_value)
+	expect(get(store)).toBe(test_value)
+}
 
-test('current_page_category', () => {
-	expect(get(current_page_category)).toBe('')
+const tests = [
+	{ name: 'title', store: current_page_title, initial_value: '', test_value: 'Home' },
+	{ name: 'category', store: current_page_category, initial_value: '', test_value: 'dev' },
+]
 
-	current_page_category.set('dev')
-	expect(get(current_page_category)).toBe('dev')
+tests.forEach(({ name, store, initial_value, test_value }) => {
+	test(`${name}`, () => {
+		test_store(store, initial_value, test_value)
+	})
 })
