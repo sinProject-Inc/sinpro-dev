0024c347da50d341a4815195b5272814e4ccb8a8
sinYa Iwasaki <iwasaki@sinproject.net>
2023-08-25 09:38:45 +0900

    Refactor markdown_code_element.test.ts #234

--- src/lib/docs/markdown_code_element.test.ts
+++ src/lib/docs/markdown_code_element.test.ts
@@ -1,75 +1,49 @@
 import { expect, test } from 'vitest'
 import { MarkdownCodeElement } from './markdown_code_element'
 
+type Params = [string, string, string, string]
+
+const normal_params: Params = ['javascript', 'path/to/file.js', 'Test Title', 'console.log("test")']
+const no_path_params: Params = ['javascript', '', 'Test Title', 'console.log("test")']
+const talk_file_params: Params = ['ts', '[talk]vite.config.js', '', 'console.log("test")']
+const bash_params: Params = ['bash', '', '', 'console.log("test")']
+const unknown_params: Params = ['', '', '', 'console.log("test")']
+
 test('MarkdownCodeElement generates correct title element', async () => {
-	const markdown_element = new MarkdownCodeElement(
-		'javascript',
-		'path/to/file.js',
-		'Test Title',
-		'console.log("test")'
-	)
+	const html = new MarkdownCodeElement(...normal_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('Test Title')
-	expect(generated_element).toContain('path/to/file.js')
+	expect(html).toContain('Test Title')
+	expect(html).toContain('path/to/file.js')
 })
 
 test('MarkdownCodeElement generates correct code content', async () => {
-	const markdown_element = new MarkdownCodeElement(
-		'javascript',
-		'path/to/file.js',
-		'Test Title',
-		'console.log("test")'
-	)
+	const html = new MarkdownCodeElement(...normal_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('console.log("test")')
+	expect(html).toContain('console.log("test")')
 })
 
 test('MarkdownCodeElement handles missing filename', async () => {
-	const markdown_element = new MarkdownCodeElement(
-		'javascript',
-		'',
-		'Test Title',
-		'console.log("test")'
-	)
+	const html = new MarkdownCodeElement(...no_path_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('Test Title')
-	expect(generated_element).not.toContain('path/to/file.js')
+	expect(html).toContain('Test Title')
+	expect(html).not.toContain('path/to/file.js')
 })
 
 test('talk link', async () => {
-	const markdown_element = new MarkdownCodeElement(
-		'ts',
-		'[talk]vite.config.js',
-		'',
-		'console.log("test")'
-	)
+	const html = new MarkdownCodeElement(...talk_file_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('[talk]vite.config.js')
-	expect(generated_element).toContain(
-		'https://github.com/sinProject-Inc/talk/blob/main/vite.config.js'
-	)
+	expect(html).toContain('[talk]vite.config.js')
+	expect(html).toContain('https://github.com/sinProject-Inc/talk/blob/main/vite.config.js')
 })
 
 test('talk link', async () => {
-	const markdown_element = new MarkdownCodeElement('bash', '', '', 'console.log("test")')
+	const html = new MarkdownCodeElement(...bash_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('Bash')
+	expect(html).toContain('Bash')
 })
 
 test('unknown language', async () => {
-	const markdown_element = new MarkdownCodeElement('', '', '', 'console.log("test")')
+	const html = new MarkdownCodeElement(...unknown_params).generate()
 
-	const generated_element = markdown_element.generate()
-
-	expect(generated_element).toContain('UNKNOWN')
+	expect(html).toContain('UNKNOWN')
 })
