df872706f02f6e65a842e1c0f094ce46c2e6b86d
sinYa Iwasaki <iwasaki@sinproject.net>
2023-09-15 14:42:38 +0900

    Refactoring test cods using GPT-4 #291

--- src/lib/docs/current_page_store.test.ts
+++ src/lib/docs/current_page_store.test.ts
@@ -2,20 +2,25 @@ import { get, type Writable } from 'svelte/store'
 import { expect, test } from 'vitest'
 import { current_page_category, current_page_title } from './current_page_store'
 
-function test_store<T>(store: Writable<T>, initial_value: T, test_value: T): void {
-	expect(get(store)).toBe(initial_value)
-
-	store.set(test_value)
-	expect(get(store)).toBe(test_value)
+type Test<T> = {
+	name: string
+	store: Writable<T>
+	initial_value: T
+	test_value: T
 }
 
-const tests = [
+function test_store<T>({ name, store, initial_value, test_value }: Test<T>): void {
+	test(`${name} should be set correctly`, () => {
+		expect(get(store)).toBe(initial_value)
+
+		store.set(test_value)
+		expect(get(store)).toBe(test_value)
+	})
+}
+
+const tests: Test<string>[] = [
 	{ name: 'title', store: current_page_title, initial_value: '', test_value: 'Home' },
 	{ name: 'category', store: current_page_category, initial_value: '', test_value: 'dev' },
 ]
 
-tests.forEach(({ name, store, initial_value, test_value }) => {
-	test(`${name}`, () => {
-		test_store(store, initial_value, test_value)
-	})
-})
+tests.forEach(test_store)
