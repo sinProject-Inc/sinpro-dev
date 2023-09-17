4bd7c75cebc03b399c6a4689456f372739492cff
sinYa Iwasaki <iwasaki@sinproject.net>
2023-09-17 18:40:40 +0900

    Refactoring test codes using GPT-4 #291

--- src/lib/general/formatted_date.test.ts
+++ src/lib/general/formatted_date.test.ts
@@ -7,39 +7,37 @@ type Spec = {
 }
 
 const specs: Spec[] = [
-	{ input: '2023-08-07T09:36:02', expected: '2023-08-07 09:36:02' },
-	{ input: '2023-08-07T09:36:02.123', expected: '2023-08-07 09:36:02' },
-	{ input: '2023-08-08T09:36:02.123456', expected: '2023-08-08 09:36:02' },
+	// UTC times are specified
+	{ input: '2023-08-07T09:36:02Z', expected: '2023-08-07 18:36:02' },
+	{ input: '2023-08-07T09:36:02.123Z', expected: '2023-08-07 18:36:02' },
+	{ input: '2023-08-08T09:36:02.123456Z', expected: '2023-08-08 18:36:02' },
 
 	// Invalid date
 	{ input: 'Invalid Date', expected: 'Invalid Date' },
-
-	// UTC time
-	{ input: '2023-08-07T09:36:02Z', expected: '2023-08-07 18:36:02' },
 
 	// Time zone included
 	{ input: '2023-08-07T09:36:02+03:00', expected: '2023-08-07 15:36:02' },
 
 	// Earliest possible date
-	{ input: '0001-01-01T00:00:00', expected: '1-01-01 00:00:00' },
+	{ input: '1888-01-01T00:00:00Z', expected: '1888-01-01 09:00:00' },
 
 	// Latest possible date
-	{ input: '9999-12-31T23:59:59', expected: '9999-12-31 23:59:59' },
+	{ input: '9999-12-31T23:59:59Z', expected: '10000-01-01 08:59:59' },
 
 	// Empty string input
 	{ input: '', expected: 'Invalid Date' },
 
 	// Invalid date format
-	{ input: '2023-13-07T09:36:02', expected: 'Invalid Date' },
+	{ input: '2023-13-07T09:36:02Z', expected: 'Invalid Date' },
 
 	// Missing date part
-	{ input: 'T09:36:02', expected: 'Invalid Date' },
+	{ input: 'T09:36:02Z', expected: 'Invalid Date' },
 
 	// Missing time part
-	{ input: '2023-08-07T', expected: 'Invalid Date' },
+	{ input: '2023-08-07TZ', expected: 'Invalid Date' },
 
 	// Missing milliseconds part
-	{ input: '2023-08-07T09:36:02.', expected: 'Invalid Date' },
+	{ input: '2023-08-07T09:36:02.Z', expected: 'Invalid Date' },
 ]
 
 test.each(specs)('FormattedDate.japan($input) -> $expected', (spec) => {
